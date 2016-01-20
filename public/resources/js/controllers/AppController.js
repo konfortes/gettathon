df792677052f1adcd71d78bc3905153c09e5e466
GTkonfortes <ronenk@gettaxi.com>
2016-01-20 14:21:43 +0200

    init state

--- public/resources/js/controllers/AppController.js
+++ public/resources/js/controllers/AppController.js
@@ -3,15 +3,21 @@
     var AppController = function($scope) {
         var ref = new Firebase("https://gettathon.firebaseio.com/rooms");
 
-        $scope.roomBSrc = "/resources/img/free.jpeg"
-        
+        ref.once("value", function(data) {
+          initRooms(data.val());
+        });
+
         ref.on("child_changed", function(snapshot) {
-            val = snapshot.val();
-            // occupiedOrFree = val.occupied ? 'occupied' : 'free'
-            // alert('room ' + val.name + ' is now ' + occupiedOrFree);  
+            val = snapshot.val(); 
             $scope.roomBSrc = val.occupied ? "/resources/img/occupied.jpeg" : "/resources/img/free.jpeg"
             $scope.$apply()
         });
+
+        function initRooms(rooms) {
+            var occupied = rooms['b'].occupied
+            $scope.roomBSrc = occupied ? "/resources/img/occupied.jpeg" : "/resources/img/free.jpeg"
+            $scope.$apply()
+        }
     }
 
     app.controller("AppController", AppController);
